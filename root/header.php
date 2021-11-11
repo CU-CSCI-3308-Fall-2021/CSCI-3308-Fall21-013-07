ade7d5662c41f5b66b1ce060064f047c32c8f688
tylerhand1 <tyha7462@colorado.edu>
2021-11-11 15:45:08 -0700

    Worked on developing infrastructure for storing and displaying maps

--- root/header.php
+++ root/header.php
@@ -8,17 +8,32 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Map Creator</title>
-        <link rel="stylesheet" type="text/css" href="style.css"/>
+        <?php
+            // Grab current URL
+            $currPage = substr($_SERVER["SCRIPT_NAME"],strrpos($_SERVER["SCRIPT_NAME"],"/")+1);
+            if ($currPage == "map.php") {
+                echo '<link rel="stylesheet" type="text/css" href="../style.css"/>';
+            } else {
+                echo '<link rel="stylesheet" type="text/css" href="style.css"/>';
+            }
+        ?>
     </head>
     <body>
         <header>
             <nav>
                 <ul>
-                    <li><a href="index.php" class="active">Home</a></li>
-                    <li><a href="#">Create</a></li>
-                    <li><a href="search.php">Search Map</a></li>
-                    <li><a href="#">About</a></li>
                     <?php
+                        if ($currPage == "map.php") {
+                            echo '<li><a href="../index.php" class="active">Home</a></li>
+                                <li><a href="../#">Create</a></li>
+                                <li><a href="../search.php">Search Map</a></li>
+                                <li><a href="../#">About</a></li>';
+                            } else {
+                            echo '<li><a href="index.php" class="active">Home</a></li>
+                                <li><a href="#">Create</a></li>
+                                <li><a href="search.php">Search Map</a></li>
+                                <li><a href="#">About</a></li>';
+                        }
                         if(isset($_SESSION['userId'])) {
                             $userName = $_SESSION['userUid'];
                             echo "<div class='user-logout-container'>";
