cae0407502885ba488dd13e14bff3d705757a297
tylerhand1 <tyha7462@colorado.edu>
2021-11-18 15:28:59 -0700

    Fixed page routing error

--- All project code components/root/header.php
+++ All project code components/root/header.php
@@ -14,12 +14,8 @@
             $currPage = substr($_SERVER["SCRIPT_NAME"],strrpos($_SERVER["SCRIPT_NAME"],"/")+1);
             // uniqid() used to force CSS to be updated
             echo '<link rel="stylesheet" type="text/css" href="/CSCI-3308-Fall21-013-07/root/includes/css/style.css?'.uniqid().'"/>';
-            if ($currPage == "create.php") {
-                echo '<script type="text/javascript" src="js/create.js"></script>';
-                echo '<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>';
-            }
-            if($currPage == "editdrawing.php" || $currPage == "map.php") {
-                echo '<script type="text/javascript" src="../js/create.js"></script>';
+            if($currPage == "create.php" || $currPage == "editdrawing.php" || $currPage == "map.php") {
+                echo '<script type="text/javascript" src="/CSCI-3308-Fall21-013-07/root/js/create.js"></script>';
                 echo '<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>';
             }
         ?>
@@ -38,21 +34,18 @@
             <nav>
                 <ul>
                     <?php
-                        if ($currPage == "map.php" || $currPage == "editdrawing.php") {
-                            echo '<li><a href="../index.php" class="active">Home</a></li>
-                                <li><a href="../create.php">Create</a></li>
-                                <li><a href="../search.php">Search Drawing</a></li>';
-                            } else {
-                                echo '<li><a href="index.php" class="active">Home</a></li>
-                                    <li><a href="create.php">Create</a></li>
-                                    <li><a href="search.php">Search Drawing</a></li>';
-                        }
+                        echo '<li><a href="/CSCI-3308-Fall21-013-07/root/index.php" class="active">Home</a></li>
+                            <li><a href="/CSCI-3308-Fall21-013-07/root/create.php">Create</a></li>
+                            <li><a href="/CSCI-3308-Fall21-013-07/root/search.php">Search Drawing</a></li>';
                         if(isset($_SESSION['userId'])) {
                             $userName = $_SESSION['userUid'];
                             echo "<div class='user-logout-container'>";
                             if ($currPage == "map.php" || $currPage == "editdrawing.php") {
                                 echo "<li class='user-profile-link'><a href='../profile.php?user=$userName'>$userName</a></li>";
                                 echo "<li class='login-btn'><a href='../includes/logout.inc.php'>Logout</a></li>";
+                            } else if ($currPage == "404.php") {
+                                echo "<li class='user-profile-link'><a href='/CSCI-3308-Fall21-013-07/root/profile.php?user=$userName'>$userName</a></li>";
+                                echo "<li class='login-btn'><a href='/CSCI-3308-Fall21-013-07/root/includes/logout.inc.php'>Logout</a></li>";
                             } else {
                                 echo "<li class='user-profile-link'><a href='profile.php?user=$userName'>$userName</a></li>";
                                 echo "<li class='login-btn'><a href='includes/logout.inc.php'>Logout</a></li>";
@@ -60,11 +53,7 @@
                             echo "</div>";
                         } else {
                             echo "<div class='user-logout-container'>";
-                            if ($currPage == "map.php" || $currPage == "editdrawing.php") {
-                                echo "<li class='login-btn'><a href='../signup.php'>Sign Up</a></li>";
-                            } else {
-                                echo "<li class='login-btn'><a href='signup.php'>Sign Up</a></li>";
-                            }
+                            echo "<li class='login-btn'><a href='/CSCI-3308-Fall21-013-07/root/signup.php'>Sign Up</a></li>";
                             echo "</div>";
                         }
                     ?>
