1edea87fa4e6ea0ae2e261b655615f32171c57fc
tylerhand1 <tyha7462@colorado.edu>
2021-11-29 18:14:25 -0700

    Updated repo structure

--- All project code components/root/includes/drawings.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE drawings (
-    drawingID int AUTO_INCREMENT,
-    username varchar(50),
-    drawingName varchar(50) BINARY,
-    dateModified DATETIME,
-    fileName varchar(50),
-    PRIMARY KEY (drawingID)
-);
